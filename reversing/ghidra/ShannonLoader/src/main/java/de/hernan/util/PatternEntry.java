c3df011ede94773d32ba1e90d4761703ab32b1d3
Grant Hernandez <grant.h.hernandez@gmail.com>
2021-03-09 18:43:56 -0800

    Recovery scatterload table bounds using disassembly + Pcode

--- reversing/ghidra/ShannonLoader/src/main/java/de/hernan/util/PatternEntry.java
+++ reversing/ghidra/ShannonLoader/src/main/java/de/hernan/util/PatternEntry.java
@@ -1,18 +1,35 @@
 package de.hernan.util;
 
 public class PatternEntry {
-  public String pattern;
-  public int offset;
+  // what the pattern is trying to find at a high-level
+  public static enum PatternType {
+    GENERIC, STRUCT, DATA16, DATA32, DATA64,
+    CODE16, CODE32, CODE64, STRING
+  };
+
+  public final String pattern;
+  public final PatternType type;
+  public final int offset;
 
   public PatternEntry(String pattern)
   {
-    this.pattern = pattern;
-    this.offset = 0;
+    this(pattern, PatternType.GENERIC);
+  }
+
+  public PatternEntry(String pattern, PatternType type)
+  {
+    this(pattern, type, 0);
   }
 
   public PatternEntry(String pattern, int offset)
   {
+    this(pattern, PatternType.GENERIC, offset);
+  }
+
+  public PatternEntry(String pattern, PatternType type, int offset)
+  {
     this.pattern = pattern;
     this.offset = offset;
+    this.type = type;
   }
 }

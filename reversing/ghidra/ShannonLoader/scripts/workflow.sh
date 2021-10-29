3fc92eb5e7eaf4e2527489e9b610f3a69570b30f
Grant Hernandez <grant.h.hernandez@gmail.com>
2021-10-29 10:19:41 -0400

    ShannonLoader: add workflow script

---
+++ reversing/ghidra/ShannonLoader/scripts/workflow.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eu
+
+if [ $# -ne 2 ]; then
+  echo "usage: $0 modem.bin ghidra_project_path"
+  exit 1
+fi
+
+FIRMWARE=$1
+PROJECT=$2
+
+echo "====> Building"
+./gradlew
+
+DISTS="./dist/*.zip"
+DISTS=( $DISTS )
+
+if [ ${#DISTS[@]} -ne 1 ]; then
+  echo "Multiple dist ZIP files! Delete all but the latest"
+  exit 1
+fi
+
+DIST="${DISTS[0]}"
+
+BASE="$GHIDRA_INSTALL_DIR"
+INSTALL_DIR="$BASE/Ghidra/Extensions/"
+
+echo "====> Installing $DIST -> $INSTALL_DIR"
+
+unzip -o "$DIST" -d "$INSTALL_DIR"
+
+if [ ! -d "$PROJECT" ]; then
+  echo "====> Making temporary Ghidra project $PROJECT"
+  mkdir -p "$PROJECT"
+else
+  echo "====> Reusing Ghidra project $PROJECT"
+fi
+
+IMPORT_AS=$(basename "$FIRMWARE")
+echo "====> Imported binary will be called $IMPORT_AS in $PROJECT"
+
+echo "====> Running Ghidra headless for $FIRMWARE"
+./scripts/ShannonFirmwareProcess.py "$PROJECT" "$IMPORT_AS" "$FIRMWARE"
+
+exit 0
